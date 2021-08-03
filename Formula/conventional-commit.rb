fec1ecc32f3054ed52d51d2fa47e636e6d958f61
Poorshad <p.shaddel@gmail.com>
2021-08-04 00:53:35 +0430

    feat: :zap: Add Formula for Homebrew
    Added a ruby file for homebrew

---
+++ Formula/conventional-commit.rb
@@ -0,0 +1,9 @@
+class Hello < Formula
+    desc "This is minimal cli that will help you use conventional commit in your project"
+    homepage "https://github.com/pshaddel/homebrew-conventional-commit"
+    url "https://github.com/pshaddel/homebrew-conventional-commit/releases/download/v1.0.0-alpha/conventional-commit-macos-x64.tar.gz"
+    sha256 "86d8874a66de9220f9cc03379f65e7805c2d2fce76a1b9646ba492cfd4065d3f"
+    version "1.0.0"  def install
+      bin.install "conventional-commit"
+    end
+  end
