abaf9cd4ca872ed2b471d26e992e60ecd9de90c2
Poorshad <p.shaddel@gmail.com>
2021-08-04 01:58:02 +0430

    fix: :bug: Better pacakge name

--- Formula/conventionalcommit.rb
+++ Formula/conventionalcommit.rb
@@ -2,9 +2,9 @@ class Conventionalcommit < Formula
     desc "This is minimal cli that will help you use conventional commit in your project"
     homepage "https://github.com/pshaddel/homebrew-conventionalcommit"
     url "https://github.com/pshaddel/homebrew-conventionalcommit/releases/download/v1.0.0-alpha/conventionalcommit-macos-x64.tar.gz"
-    sha256 "6531354b956f1dc6bfff1959c0cea4fc1d5560021c6fb3190dde64c298202ad8"
+    sha256 "9f8531877fe4c8b562f06291cee62a0794b8f45aec3cc324daf796ac92759f66"
     version "1.0.0"
     def install
-      bin.install "conventionalcommit-node14-macos-x64"
+      bin.install "conventionalcommit"
     end
   end
