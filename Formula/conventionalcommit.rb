2784e7c882f991fff9fa81f795461455e5c4f029
Poorshad <p.shaddel@gmail.com>
2021-08-04 01:44:23 +0430

    Change hash of file

--- Formula/conventionalcommit.rb
+++ Formula/conventionalcommit.rb
@@ -2,7 +2,7 @@ class Conventionalcommit < Formula
     desc "This is minimal cli that will help you use conventional commit in your project"
     homepage "https://github.com/pshaddel/homebrew-conventionalcommit"
     url "https://github.com/pshaddel/homebrew-conventionalcommit/releases/download/v1.0.0-alpha/conventionalcommit-macos-x64.tar.gz"
-    sha256 "86d8874a66de9220f9cc03379f65e7805c2d2fce76a1b9646ba492cfd4065d3f"
+    sha256 "6531354b956f1dc6bfff1959c0cea4fc1d5560021c6fb3190dde64c298202ad8"
     version "1.0.0"
     def install
       bin.install "conventional-commit"
