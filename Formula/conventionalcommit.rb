2892a97c14c3c7cc53dddae93fbb40dc128d18c6
Poorshad <p.shaddel@gmail.com>
2021-08-04 01:08:53 +0430

    Rename whole repository

--- Formula/conventionalcommit.rb
+++ Formula/conventionalcommit.rb
@@ -1,10 +1,10 @@
 class Conventionalcommit < Formula
     desc "This is minimal cli that will help you use conventional commit in your project"
-    homepage "https://github.com/pshaddel/homebrew-conventional-commit"
-    url "https://github.com/pshaddel/homebrew-conventional-commit/releases/download/v1.0.0-alpha/conventional-commit-macos-x64.tar.gz"
+    homepage "https://github.com/pshaddel/homebrew-conventionalcommit"
+    url "https://github.com/pshaddel/homebrew-conventionalcommit/releases/download/v1.0.0-alpha/conventionalcommit-macos-x64.tar.gz"
     sha256 "86d8874a66de9220f9cc03379f65e7805c2d2fce76a1b9646ba492cfd4065d3f"
     version "1.0.0"
     def install
-      bin.install "conventional-commit"
+      bin.install "conventionalcommit"
     end
   end
--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# conventional-commit-cli
+# conventionalcommit-cli
+
 This is minimal cli that will help you use conventional commit in your project.
 
 Using [conventional commit](https://www.conventionalcommits.org/en/v1.0.0/) is one of the best way we can organize our commits so we can find out what happend in that commit. This is a minimal cli tool that will help you commit using this convention.
 After implementing this cli I found out that commitizen has a realy similar cli but it needs a commitizen config file. So I think I keep developing this tool till I'll be happy with it.
-
-
-
